b6f3915115bc21246f8f16538b36240b9f5c29d4
Bogdan <krabog@gmail.com>
2019-06-20 19:30:08 +0300

    commit to branch-1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int  a = 0;
+        int a = 0;
         int b = 1;
 
-        int s = a+b;
+        int s = a + b;
 
         System.out.println(s);
+        System.out.println(s+" привет");
     }
 }
